d5ea8972f975e61a3ccc7e71f1be29688ba7331d
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2026-08-11 11:07:27 +0530

    remove live.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {},
+  },
+  experimental: {
+    turbo: {},
+  },
   eslint: { ignoreDuringBuilds: true },
 
   devIndicators: false,
